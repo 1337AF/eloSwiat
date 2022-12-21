247a8bdda4728d88668c0c93170de91a5dde4499
1337AF <100231204+1337AF@users.noreply.github.com>
2022-12-22 00:22:08 +0100

    Delete nested_for.py

--- nested_for.py
+++
@@ -1,8 +0,0 @@
-# Tutaj sposób jak wydrukować zobie "E" bez mnożenia stringu ( z neta)
-
-numbers = [5, 1, 5, 1, 5]
-for x in numbers:
-    output = ''
-    for y in range(x):
-        output += '*'
-    print(output)
